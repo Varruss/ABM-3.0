4022d65d25bd98a48312988d7be816ab4cb951f3
Jonas <jonas.gregory@outlook.com>
2024-08-23 08:58:50 -0500

    new

--- ABM 3.0/JonasView.swift
+++ ABM 3.0/JonasView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct JonasView: View {
+    @State private var showingAlertlie = false
+    @State private var showingAlerttrue1 = false
+    @State private var showingAlerttrue2 = false
+    
     var body: some View {
+        
         VStack{
             VStack{
                 Text("Jonas Gregory")
@@ -21,13 +26,42 @@ struct JonasView: View {
                     .padding()
             }
             .padding(50)
-           
+            
             
             Divider()
             VStack {
                 Text("Two Truths and a Lie")
                     .font(.title)
                     .padding()
+                    .underline()
+                Button(action: {
+                    showingAlertlie = true
+                }, label: {
+                    Text("I Have a cat named Oliver")
+                        .padding()
+                        .foregroundColor(.black)
+                        .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).border(Color.black, width: 6))
+                    
+                })
+                Button(action: {
+                    showingAlerttrue1 = true
+                }, label: {
+                    Text("I have broken 2 toes")
+                        .padding()
+                        .foregroundColor(.black)
+                        .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).border(Color.black, width: 6))
+                })
+                Button(action: {
+                    showingAlerttrue2 = true
+                }, label: {
+                    Text("I have visited 5 different countries")
+                        .padding()
+                        .foregroundColor(.black)
+                        .background(RoundedRectangle(cornerRadius: 25).fill(Color.white).border(Color.black, width: 6))
+                })
+            }
+            .alert("That's the lie", isPresented: $showingAlertlie) {
+                Button("OK", role: .cancel) { }
             }
         }
     }
