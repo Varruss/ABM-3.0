22928d05db4440a9914af6392e1cfa18b206e886
Jonas <jonas.gregory@outlook.com>
2024-08-23 08:29:15 -0500

    Initial Commit

---
+++ ABM 3.0/ABM_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ABM_3_0App.swift
+//  ABM 3.0
+//
+//  Created by Jonas R. Gregory on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ABM_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
